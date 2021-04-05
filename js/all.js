bd2b352ab9bc2f9c2cb47510a2ba6af925d97605
Aaron <aaron@gmail.com.tw>
2021-04-05 15:19:14 +0800

    hw7

--- js/all.js
+++ js/all.js
@@ -1,35 +1,48 @@
 let locationSelect = document.querySelector("#locationSelect");
-locationSelect.addEventListener("change", changeTicketList);
+locationSelect.addEventListener("change", renderLists);
 let searchResult = document.querySelector("#searchResult");
 let ticketLists = document.querySelector("#ticketLists");
+let datas = [];
 const submitButton = document.querySelector("#submitButton");
 submitButton.addEventListener("click", insertData);
+getData();
 function insertData() {
     let data = {
-        "id": datas.length +1,
-        "name": document.querySelector("#ticketName").value,
-        "imgUrl": document.querySelector("#url").value,
-        "area": document.querySelector("#location").value,
-        "description": document.querySelector("#ticketDescription").value,
-        "group": parseInt(document.querySelector("#ticketNumber").value),
-        "price": parseInt(document.querySelector("#ticketPrice").value),
-        "rate": parseInt(document.querySelector("#ticketLevel").value)
+        id: datas.length,
+        name: document.querySelector("#ticketName").value,
+        imgUrl: document.querySelector("#url").value,
+        area: document.querySelector("#location").value,
+        description: document.querySelector("#ticketDescription").value,
+        group: parseInt(document.querySelector("#ticketNumber").value),
+        price: parseInt(document.querySelector("#ticketPrice").value),
+        rate: parseInt(document.querySelector("#ticketLevel").value)
     }
-    
-    if(checkData(data))
-    {
+    if (checkData(data)) {
         datas.push(data);
     }
-
+    cleanInput();
 }
-function changeTicketList() {
+//新增完後刪除
+function cleanInput() {
+    document.querySelector("#ticketName").valu = "";
+    document.querySelector("#url").value = "";
+    document.querySelector("#location").value = "";
+    document.querySelector("#ticketDescription").value = "";
+    document.querySelector("#ticketNumber").value = "";
+    document.querySelector("#ticketPrice").value = "";
+    document.querySelector("#ticketLevel").value = "";
+}
+//顯示資料
+function renderLists() {
     let html = "";
     let results = datas.filter(function (item) {
-        if (locationSelect.value == "全部地區") {
+        if (locationSelect.value == "全部地區" || locationSelect.value == "") {
+
             return item.area;
         } else {
             return locationSelect.value == item.area;
         }
+
     });
     results.forEach(function (item) {
         html += `<li><div class="titcketLocation">${item.area}</div>
@@ -49,31 +62,33 @@ function changeTicketList() {
     ticketLists.innerHTML = html;
     searchResult.textContent = `本次搜尋共 ${results.length} 筆資料`;
 }
+
+//檢查資料
 function checkData(data) {
-    
+
     let result = "";
 
-    
+
     if (data.name.trim() == "") {
-        result += "請填寫名稱";
+        result += "請填寫名稱\n";
     }
     if (data.imgUrl.trim() == "") {
-        result += "請填寫網址";
+        result += "請填寫網址\n";
     }
     if (data.area.trim() == "") {
-        result += "請填寫地區";
+        result += "請填寫地區\n";
     }
-    if (data.price == "") {
-        result += "請填寫金額";
+    if (isNaN(data.price)) {
+        result += "請填寫金額\n";
     }
-    if (data.group == "") {
-        result += "請填寫數量";
+    if (isNaN(data.group)) {
+        result += "請填寫數量\n";
     }
-    if (data.rate == "") {
-        result += "請填寫組數";
+    if (isNaN(data.rate)) {
+        result += "請填寫組數\n";
     }
     if (data.description.trim() == "") {
-        result += "請填寫描述";
+        result += "請填寫描述\n";
     }
     if (result != "") {
         alert(result);
@@ -83,51 +98,68 @@ function checkData(data) {
         return true;
     }
 }
-let datas = [
-    {
-        "id": 0,
-        "name": "肥宅心碎賞櫻3日",
-        "imgUrl": "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
-        "area": "高雄",
-        "description": "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
-        "group": 87,
-        "price": 1400,
-        "rate": 10
-    },
-    {
-        "id": 1,
-        "name": "貓空纜車雙程票",
-        "imgUrl": "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-        "area": "台北",
-        "description": "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
-        "group": 99,
-        "price": 240,
-        "rate": 2
-    },
-    {
-        "id": 2,
-        "name": "台中谷關溫泉會1日",
-        "imgUrl": "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-        "area": "台中",
-        "description": "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
-        "group": 20,
-        "price": 1765,
-        "rate": 7
-    },
-    {
-        "id": 3,
-        "name": "台中谷關溫泉會1日",
-        "imgUrl": "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-        "area": "台中",
-        "description": "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
-        "group": 20,
-        "price": 1765,
-        "rate": 7
-    }
-];
-/*
-axios.get('https://raw.githubusercontent.com/hexschool/js-training/main/travelAPI-lv1.json')
-    // 如果連接成功，可以用 then() 處理傳回來的值，以下程式碼將回傳結果儲存於 responce。
-  .then(function (response) {
-    	console.log(response);
-  });*/
+//取得資料
+function getData() {
+    axios.get('https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json')
+        // 如果連接成功，可以用 then() 處理傳回來的值，以下程式碼將回傳結果儲存於 responce。
+        .then(function (response) {
+            datas = response.data.data;
+            renderLists();
+            renderDount();
+        });
+}
+//圓餅圖
+function renderDount() {
+    let ary = [];
+    let areaObj = {};
+    //計算每個區域出現次數
+    datas.forEach(function (item) {
+        if (areaObj[item.area] == undefined) {
+            areaObj[item.area] = 1;
+        }
+        else {
+            areaObj[item.area] += 1;
+        }
+    });
+    //將物件次數轉換成陣列
+    const aryArea = Object.keys(areaObj);
+    aryArea.forEach(function (item) {
+        let newObj = [];
+        newObj.push(item);
+        let num = areaObj[item];
+        newObj.push(num);
+
+        ary.push(newObj);
+
+    });
+    var chart = c3.generate({
+        size: {
+            height: 176,
+            width: 300
+        },
+        data: {
+            labels: false,
+            columns: ary,
+            colors: {
+                '台北': '#26C0C7',
+                '台中': '#5151D3',
+                '高雄': '#E68618'
+            },
+            type: 'donut',
+            onclick: function (d, i) { console.log("onclick", d, i); },
+            onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+            onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+        },
+        donut: {
+            title: "套票地區比重",
+            width: 15,
+            label: {
+                format: function (value) { return; }
+            }
+        },
+        bindto: '#chart', // HTML 元素綁定
+    });
+}
+
+
+
